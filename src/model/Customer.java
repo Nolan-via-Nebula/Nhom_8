e74e05222ad173926f9d3a47cd489926490bd1b7
Nolan-via-Nebula <muchdiagram2708@gmail.com>
2025-11-24 01:43:24 +0700

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -3,21 +3,24 @@ package model;
 public class Customer extends Person {
     // Constructor
     public Customer() {
-		super();
-	}
+        super();
+    }
 
-	public Customer(String id, String name, String phoneNumber) {
-		super(id, name, phoneNumber);
-	}
+    public Customer(String id, String name, String phoneNumber) {
+        super(id, name, phoneNumber);
+    }
 
-	// Override 
-	@Override
-	public String getRole() {
-		return "Customer";
-	}
-	@Override
-	public String toString() {
-		return "Customer " + super.toString();
-	}
-	
+    @Override
+    public String getRole() {
+        return "Customer";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s | %s",
+                getId(),
+                getName(),
+                getPhoneNumber()
+        );
+    }
 }
