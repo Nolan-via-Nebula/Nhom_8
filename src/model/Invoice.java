d2b71b8f117aca349f7faf25f10468dd52b8b396
Nolan-via-Nebula <muchdiagram2708@gmail.com>
2025-11-24 01:43:18 +0700

    Update Invoice.java

--- src/model/Invoice.java
+++ src/model/Invoice.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Invoice {
-    // Attribute
+
     private String invoiceId;
     private Customer customer;
     private Employee employee;
@@ -13,21 +13,28 @@ public class Invoice {
 
     private List<InvoiceItem> items = new ArrayList<>();
     
-    // Constructor
+	// Constructor 
     public Invoice() {
-		super();
-	}
+        super();
+    }
 
-	public Invoice(String invoiceId, Customer customer, Employee employee, LocalDate date, List<InvoiceItem> items) {
-		super();
-		this.invoiceId = invoiceId;
-		this.customer = customer;
-		this.employee = employee;
-		this.date = date;
-		this.items = new ArrayList<>();
-	}
+    public Invoice(String invoiceId, Customer customer, Employee employee, LocalDate date, List<InvoiceItem> items) {
+        this.invoiceId = invoiceId;
+        this.customer = customer;
+        this.employee = employee;
+        this.date = date;
+        this.items = items;
+    }
 
-	// Thêm item
+    public Invoice(String invoiceId, Customer customer, Employee employee, LocalDate date) {
+        this.invoiceId = invoiceId;
+        this.customer = customer;
+        this.employee = employee;
+        this.date = date;
+        this.items = new ArrayList<>();
+    }
+
+    // Thêm item
     public void addItem(Product product, int quantity) {
         InvoiceItem item = new InvoiceItem(product, quantity);
         items.add(item);
@@ -42,59 +49,60 @@ public class Invoice {
         return sum;
     }
 
-    // Get/Set
+    // Getter – Setter
     public String getInvoiceId() {
-		return invoiceId;
-	}
+        return invoiceId;
+    }
 
-	public void setInvoiceId(String invoiceId) {
-		this.invoiceId = invoiceId;
-	}
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
 
-	public Customer getCustomer() {
-		return customer;
-	}
+    public Customer getCustomer() {
+        return customer;
+    }
 
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 
-	public Employee getEmployee() {
-		return employee;
-	}
+    public Employee getEmployee() {
+        return employee;
+    }
 
-	public void setEmployee(Employee employee) {
-		this.employee = employee;
-	}
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 
-	public LocalDate getDate() {
-		return date;
-	}
+    public LocalDate getDate() {
+        return date;
+    }
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
     public List<InvoiceItem> getItems() {
-		return items;
-	}
+        return items;
+    }
 
-	@Override
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("===== INVOICE =====\n");
-        sb.append("ID: ").append(invoiceId).append("\n");
-        sb.append("Date: ").append(date).append("\n");
-        sb.append("Customer: ").append(customer.getName()).append("\n");
-        sb.append("Employee: ").append(employee.getName()).append("\n\n");
+
+        sb.append("\n------- INVOICE -------\n");
+        sb.append(String.format("Invoice ID : %s\n", invoiceId));
+        sb.append(String.format("Date       : %s\n", date));
+        sb.append(String.format("Customer   : %s\n", customer.getName()));
+        sb.append(String.format("Employee   : %s\n\n", employee.getName()));
 
         sb.append("Items:\n");
         for (InvoiceItem item : items) {
             sb.append(item).append("\n");
         }
 
-        sb.append("\nTOTAL: ").append(getTotal()).append(" VND\n");
-        sb.append("===================\n");
+        sb.append(String.format("\nTOTAL AMOUNT: %.0f VND\n", getTotal()));
+        sb.append("===========================\n");
 
         return sb.toString();
     }
