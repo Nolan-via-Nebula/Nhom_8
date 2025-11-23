e10dc237ec1dd2fd39f42ea95d7208161ccf5126
Nolan-via-Nebula <muchdiagram2708@gmail.com>
2025-11-23 15:28:16 +0700

    Update Employee.java

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,32 +1,55 @@
 package model;
 
 public class Employee extends Person{
-	// Thuộc tính 
+    // Attribute
 	private String position;
     private double salary;
+    
     // Constructor
     public Employee() {
 		super();
 	}
-    
-	public Employee(String id, String name, String phoneNumber, String address) {
-		super(id, name, phoneNumber, address);
+    	
+	public Employee(String id, String name, String phoneNumber) {
+		super(id, name, phoneNumber);
 	}
 
-	public Employee(String position, double salary) {
-		super();
+	public Employee(String id, String name, String phoneNumber, String position, double salary) {
+		super(id, name, phoneNumber);
 		this.position = position;
 		this.salary = salary;
 	}
 
+	// Get/Set
+	public String getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
 	// Override 
 	@Override
 	public String getRole() {
 		return "Employee";
 	}
-	@Override
+	
 	public String toString() {
-		return "Employee " + super.toString();
-	}
+        return "[Employee] " + super.toString() +
+               ", Position: " + position +
+               ", Salary: " + salary;
+    }
 
 }
