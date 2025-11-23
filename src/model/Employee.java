59954022832cc0cfdb1e0065c3c4014719444777
Nolan-via-Nebula <muchdiagram2708@gmail.com>
2025-11-24 01:43:21 +0700

    Update Employee.java

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,55 +1,51 @@
 package model;
 
-public class Employee extends Person{
+public class Employee extends Person {
     // Attribute
-	private String position;
+    private String position;
     private double salary;
     
     // Constructor
     public Employee() {
-		super();
-	}
-    	
-	public Employee(String id, String name, String phoneNumber) {
-		super(id, name, phoneNumber);
-	}
-
-	public Employee(String id, String name, String phoneNumber, String position, double salary) {
-		super(id, name, phoneNumber);
-		this.position = position;
-		this.salary = salary;
-	}
-
-	// Get/Set
-	public String getPosition() {
-		return position;
-	}
-
-
-	public void setPosition(String position) {
-		this.position = position;
-	}
-
-
-	public double getSalary() {
-		return salary;
-	}
-
-
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	
-	// Override 
-	@Override
-	public String getRole() {
-		return "Employee";
-	}
-	
-	public String toString() {
-        return "[Employee] " + super.toString() +
-               ", Position: " + position +
-               ", Salary: " + salary;
+        super();
+    }
+        
+    public Employee(String id, String name, String phoneNumber) {
+        super(id, name, phoneNumber);
     }
 
+    public Employee(String id, String name, String phoneNumber, String position, double salary) {
+        super(id, name, phoneNumber);
+        this.position = position;
+        this.salary = salary;
+    }
+
+    // Get/Set
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    @Override
+    public String getRole() {
+        return "Employee";
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("[%s] %s | %s | %s | %,d VND",
+                getId(), getName(), position, getPhoneNumber(), (long) salary
+        );
+    }
 }
