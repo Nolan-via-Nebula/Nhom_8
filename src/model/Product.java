2f24d3bd16686215fb8efb4156521f00afd502a8
Nolan-via-Nebula <muchdiagram2708@gmail.com>
2025-11-23 15:28:00 +0700

    Create Product.java

---
+++ src/model/Product.java
@@ -0,0 +1,84 @@
+package model;
+
+public abstract class Product {
+    // Attribute
+    protected String productId;
+    protected String productName;
+    protected double price;
+    protected int quantity;
+    protected Brand brand;
+
+    // Constructor
+	public Product() {
+		super();
+	}
+
+	public Product(String productId, String productName, double price, int quantity, Brand brand) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+		this.brand = brand;
+	}
+
+	// Get/Set 
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+    
+	// Các phương thức tiện ích 
+	public void increaseQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    public boolean decreaseQuantity(int amount) {
+        if (amount > this.quantity) {
+            return false; 
+        }
+        this.quantity -= amount;
+        return true;
+    }
+	
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + ", quantity=" + quantity + ", brand=" + brand + "]";
+	}
+        
+}
